60300f1d0d3f20eda67f9a22a6f848d4f9287023
Ahasan-Habib-Abir <139943351+Ahasan-Habib-Abir@users.noreply.github.com>
2023-08-13 08:14:20 +0600

    Update QueryWriting.sql

--- QueryWriting.sql
+++ QueryWriting.sql
@@ -1,44 +1,78 @@
-SELECT E ID, E NAME, DOB, JOB TITLE,
-SALARY FROM EMP Info WHERE SALARY¡15000
-
-SELECT IN DATE, OUT DATE,
-ROOM TYPE, ROOM NO, CUS ID FROM Accommodation WHERE CUS ID
-= ’20’
+SELECT E_ID, E_NAME, DOB, JOB_TITLE, SALARY 
+FROM EMP_Info
+WHERE SALARY<15000
 
 
-SELECT CUS ID, COLUMN VALUE AS Phone No FROM CUS CON, TABLE(CUS CON.Phone No) WHERE CUS ID = ’10’;
+
+SELECT IN_DATE, OUT_DATE, ROOM_TYPE, ROOM_NO, CUS_ID
+FROM Accommodation
+WHERE CUS_ID = '20'
 
 
-SELECT AMOUNT, INVOICE NUMBER, PAY DATE FROM
-PAYMENT WHERE AMOUNT = (SELECT MAX(AMOUNT) FROM
-PAYMENT);
 
-SELECT ROUND(AVG(SALARY),2) AS ”AVERAGE SALARY”,
-AGENCY ID FROM EMP Info GROUP BY AGENCY ID
-
-SELECT CUS ID, CUS NAME, EMAIL, NVL(MEMBERSHIP LEVEL,’NO
-MEMBERSHIP’), AGENCY ID FROM CUS INFO WHERE AGENCY ID
-= ( SELECT AGENCY ID FROM AGENCY INFO WHERE AGENCY NAME
-= ’ABC’)
+SELECT CUS_ID, COLUMN_VALUE AS Phone_No
+FROM CUS_CON, TABLE(CUS_CON.Phone_No)
+WHERE CUS_ID = '10';
 
 
-SELECT * FROM AIRLINE WHERE CUS ID = (SELECT CUS ID
-FROM CUS INFO WHERE CUS NAME = ’RAISUL’)
+
+SELECT AMOUNT, INVOICE_NUMBER, PAY_DATE
+FROM PAYMENT
+WHERE AMOUNT = (SELECT MAX(AMOUNT) FROM PAYMENT);
 
 
-SELECT C.CUS NAME, T.* FROM CUS Info C, transportation
-T WHERE C.CUS ID = T.CUS ID
+
+SELECT ROUND(AVG(SALARY),2) AS "AVERAGE SALARY", AGENCY_ID
+FROM EMP_Info 
+GROUP BY AGENCY_ID
 
 
-select info.*, loc.* from Agency Info info, Agency loc loc where
-info.agency id = loc.agency id
+
+SELECT CUS_ID, CUS_NAME, EMAIL, NVL(MEMBERSHIP_LEVEL,'NO MEMBERSHIP'), AGENCY_ID
+FROM CUS_INFO
+WHERE AGENCY_ID = ( SELECT AGENCY_ID 
+                    FROM AGENCY_INFO
+                    WHERE AGENCY_NAME = 'ABC')
 
 
-create view transport as select TRANS NO,PICK LOC, DROP LOC
-from transportation where cus id = ’10’ DESC TRANSPORT
 
 
-create view EMP SAL ( NAME, MINSAL, MAXSAL) as select
-a.agency name, min(e.salary), max(e.salary) from emp info e, agency info
-a where e.agency id = a.agency id group by a.agency name DESC
-EMP SAL
+SELECT *
+FROM AIRLINE
+WHERE CUS_ID = (SELECT CUS_ID 
+                FROM CUS_INFO
+                WHERE CUS_NAME = 'RAISUL')
+
+
+
+
+SELECT C.CUS_NAME, T.*
+FROM CUS_Info C, transportation T
+WHERE C.CUS_ID = T.CUS_ID
+
+
+
+select info.*, loc.*
+from Agency_Info info, Agency_loc loc
+where info.agency_id = loc.agency_id
+
+
+
+
+create view transport
+as select  TRANS_NO,PICK_LOC, DROP_LOC
+from transportation
+where cus_id = '10'
+DESC TRANSPORT
+
+
+
+create view EMP_SAL ( NAME, MINSAL, MAXSAL)
+as select a.agency_name, min(e.salary), max(e.salary)
+from emp_info e, agency_info a
+where e.agency_id = a.agency_id
+group by a.agency_name
+DESC EMP_SAL
+
+
+
